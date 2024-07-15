199067c38b8c329c62911863e8cfc3143061395d
jacksonkwp <jackson.k.w.p@gmail.com>
2024-07-15 15:39:57 -0700

    enabled discovery client

--- gateway-server/src/main/java/com/smartHomeHub/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/smartHomeHub/gateway/GatewayServerApplication.java
@@ -2,8 +2,10 @@ package com.smartHomeHub.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
