d7e300c744fd061ec68a848eb81fe28969ed9d67
Jackson Whiteley <44081344+jacksonkwp@users.noreply.github.com>
2024-07-14 20:25:38 -0700

    git migration

---
+++ gateway-server/src/test/java/com/smartHomeHub/gateway/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartHomeHub.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
